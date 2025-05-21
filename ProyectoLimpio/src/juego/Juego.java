cb5d8c5502611b6e77b3236268702de1e3168e98
lucaso02 <lucasoobregon04@gmail.com>
2025-05-20 21:36:53 -0300

    mago basico

--- ProyectoLimpio/src/juego/Juego.java
+++ ProyectoLimpio/src/juego/Juego.java
@@ -1,8 +1,8 @@
 package juego;
 
-
+import java.awt.Image;
 import java.awt.Color;
-
+import entorno.Herramientas;
 import entorno.Entorno;
 import entorno.InterfaceJuego;
 
@@ -10,15 +10,17 @@ public class Juego extends InterfaceJuego
 {
 	// El objeto Entorno que controla el tiempo y otros
 	private Entorno entorno;
-	
+	private mago p1 ;
+	private Image fondo; // inicia la herramienta Image
 	// Variables y métodos propios de cada grupo
 	// ...
 	
 	Juego()
 	{
 		// Inicializa el objeto entorno
-		this.entorno = new Entorno(this, "Proyecto para TP", 800, 600);
-		
+		this.entorno = new Entorno(this, "Trabajo Practico: El camino de Gondolf", 1300, 800);
+		this.fondo = Herramientas.cargarImagen("cosas/fondo.png"); // Carga el fondo que esta en la carpeta "cosas"ProyectoLimpio
+		this.p1= new mago(100,100);
 		// Inicializar lo que haga falta para el juego
 		// ...
 
@@ -34,9 +36,22 @@ public class Juego extends InterfaceJuego
 	 */
 	public void tick()
 	{
+		entorno.dibujarImagen(fondo, 600, 500, 0);
 		// Procesamiento de un instante de tiempo
 		// ...
-		
+		this.p1.dibujar(entorno);
+		if(entorno.estaPresionada(entorno.TECLA_ARRIBA)) {
+			this.p1.mover(-1,0);
+		}
+		if(entorno.estaPresionada(entorno.TECLA_ABAJO)) {
+			this.p1.mover(1,0);
+		}
+		if(entorno.estaPresionada(entorno.TECLA_DERECHA)) {
+			this.p1.mover(0,1);
+		}
+		if(entorno.estaPresionada(entorno.TECLA_IZQUIERDA)) {
+			this.p1.mover(0,-1);
+		}
 	}
 	
 
